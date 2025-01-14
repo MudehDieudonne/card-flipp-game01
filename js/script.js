9205a45834f72a3889c0f88179221016ecd36a06
zamba <mukummudeh@gmail.com>
2025-01-14 14:40:29 +0100

    correction of shiffle function

--- js/script.js
+++ js/script.js
@@ -23,30 +23,34 @@ const allCards = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'A', 'B', 'C', 'D', 'E
 // const allCards = [...cardValues, ...cardValues]
 
 //Shuffle Cards
-function shuffle(array) {
+function shuffle (array) {
   for (let i = array.length - 1; i > 0; i--) {
-    const j = Math.floor(Math.random() * (i + 1));
-    [array[i], array[j]] = [array[j], array[i]];
+    const j = Math.floor(Math.random() * (i + 1))
+    console.log(`Swapping elements at indexes ${i} and ${j}`)
+    if (array[i] !== undefined && array[j] !== undefined){
+      [array[i], array[j]] = [array[j], array[i]]
+    }
   }
 }
 
 // adding card element with event listener
-function createCards() {
-  shuffle(allCards);
+function createCards () {
+  shuffle(allCards)
   allCards.forEach((value, index) => {
-    const card = document.createElement('div');
-    card.classList.add('card');
-    card.dataset.value = value;
+    const card = document.createElement('div')
+    card.classList.add('card')
+    card.dataset.value = value
     card.innerHTML = `
       <div class="card-inner">
         <div class="card-front">?</div>
         <div class="card-back">${value}</div>
       </div>
-    `;
-    card.addEventListener('click', flipCard);
-    gameBoard.appendChild(card);
-    cards.push(card);
-  });
+    `
+    console.log("beans")
+    card.addEventListener('click', flipCard)
+    gameBoard.appendChild(card)
+    cards.push(card)
+  })
 }
 
 // Timer
@@ -54,7 +58,7 @@ let secons = 0
 let minutes = 0
 let hours = 0
 let timer = null
-function stopwatch() {
+function stopwatch () {
   secons++
   if (secons === 60) {
     secons = 0
@@ -64,44 +68,44 @@ function stopwatch() {
       hours++
     }
   }
-  let h = hours < 10 ? `0${hours}` : hours
-  let m = minutes < 10 ? `0${minutes}` : minutes
-  let s = secons < 10 ? `0${secons}` : secons
+  const h = hours < 10 ? `0${hours}` : hours
+  const m = minutes < 10 ? `0${minutes}` : minutes
+  const s = secons < 10 ? `0${secons}` : secons
   timerDisplay.innerHTML = `<h2>${h}:${m}:${s}</h2>`
 }
 
-function startTimer() {
+function startTimer () {
   timer = setInterval(stopwatch, 1000)
 }
 
-function stopTimer() {
+function stopTimer () {
   clearInterval(timer)
 }
 
-//reset timer
+// reset timer
 function resetTimer() {
   clearInterval(timer)
   secons = 0
   minutes = 0
   hours = 0
-  timerDisplay.innerHTML = `<h2>00:00:00</h2>`
+  timerDisplay.innerHTML = '<h2>00:00:00</h2>'
 }
 
 // FLip Function
-function flipCard() {
+function flipCard () {
   if (flippedCards.length < 2 && !this.classList.contains('flipped')) {
     this.classList.add('flipped')
     flippedCards.push(this)
     if (flippedCards.length === 2) {
-      attempts ++
+      attempts++
       attemptsDisplay.textContent = attempts
       checkForMatch()
     }
   }
 }
 
-//check match
-function checkForMatch() {
+// check match
+function checkForMatch () {
   if (flippedCards[0].dataset.value === flippedCards[1].dataset.value) {
     matchedCard.push(...flippedCards)
     flippedCards = []
@@ -111,7 +115,7 @@ function checkForMatch() {
       completionPage.innerHTML = `<h2> Congratulations! You've matched all the cards!</h2>
         <p>Total attempts: <span id="attempts">${attempts}</span></p>
         <button id="play-again"><h2>Play Again</h2></button>`
-// Play Again
+      // Play Again
       document.getElementById('play-again').addEventListener('click', () => {
         completionPage.classList.add('hidden')
         gameBoard.innerHTML = ''
